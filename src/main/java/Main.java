6e7e2db1722ed3f17f02a24a22b82b3cb99c8f86
imsahil2001 <sahilkkr56@gmail.com>
2024-05-14 02:15:02 +0530

    added a 200OK response status code to http server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
@@ -17,6 +19,17 @@ public class Main {
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+
+       /*
+        To write data to client when it requests to server
+        OutputStream needed to be used to write the whole httpresponse set along with headers and body and write it to clientSocket
+        */
+
+       OutputStream outputStream = clientSocket.getOutputStream();
+       outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+
+       outputStream.flush();
+       clientSocket.close();
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
