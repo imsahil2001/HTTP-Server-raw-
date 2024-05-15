23c9fae27ddef30b21672dc95d1a2c19a38d79d3
imsahil2001 <sahilkkr56@gmail.com>
2024-05-15 19:23:36 +0530

    fixed typo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 public class Main {
     private static final int PORT = 4221;
     public static final String HTTP_OK_Response = "HTTP/1.1 200 OK\r\n\r\n";
-    public static final String HTTP_NotFound_Response = "HTTP/1.1 200 OK\r\n\r\n";
+    public static final String HTTP_NotFound_Response = "HTTP/1.1 404 Not Found\r\n\r\n";
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
