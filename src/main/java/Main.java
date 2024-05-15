b136217521f37749a7d02d02558d078d77bc1894
imsahil2001 <sahilkkr56@gmail.com>
2024-05-15 16:56:46 +0530

    Reading the headers and added a Not found status code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
+    private static final int PORT = 4221;
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
@@ -15,7 +15,7 @@ public class Main {
      Socket clientSocket = null;
 
      try {
-       serverSocket = new ServerSocket(4221);
+       serverSocket = new ServerSocket(PORT);
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
@@ -26,12 +26,68 @@ public class Main {
         */
 
        OutputStream outputStream = clientSocket.getOutputStream();
-       outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+       String urlPath = getHeaderData(clientSocket.getInputStream());
+       if(URLS.checkUrl(urlPath) != null)
+           outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+       else
+           outputStream.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes(StandardCharsets.UTF_8));
 
        outputStream.flush();
        clientSocket.close();
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
+  }
+
+  /*
+    Extacting and reading the header data of each request
+   */
+  private static String getHeaderData(InputStream inputStream){
+      String rawheaderData = "";
+      String urlPath = null;
+      try{
+          int c;
+          Reader reader = new InputStreamReader(inputStream);
+          while((c = reader.read()) != -1){
+              System.out.print((char)c);
+              rawheaderData += (char)c;
+              if(rawheaderData.contains("\r\n\r\n"))
+                  break;
+          }
+          //Reading the headers of a request and extracting the method, url-path & http version
+          urlPath = rawheaderData.split("\n")[0].split(" ")[1];
+
+          if(!urlPath.isEmpty())
+              return urlPath;
+          else throw new NullPointerException("Didn't find the path in the request");
+      }catch(NullPointerException nullEx){
+          System.out.println("Exception occurred :: " + nullEx);
+      }catch(Exception ex){
+          System.out.println("Exception occurred :: " + ex);
+      }
+      return null;
+  }
+
+  /*
+    Set of pre-existing hardcoded urls that supported by server
+   */
+  public enum URLS{
+      HOME_PAGE("/"),
+      HOME_PAGE_1("");
+      private final String url;
+      URLS(String url) {
+        this.url = url;
+      }
+      public String getUrl(){
+          return url;
+      }
+      public static URLS checkUrl(String url){
+          for (URLS value : values()) {
+              if (value.getUrl().equals(url)) {
+                  return value;
+              }
+          }
+          return null;
+      }
   }
 }
